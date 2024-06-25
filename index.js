95fd73c92f7ef698ad7c1130a09835371843feeb
Gyaneswar <gyaneswarrout12345@gmail.com>
2024-06-25 11:34:56 +0530

    my first commit  with '#' will be ignored, and an empty message aborts the commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/index", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/skills", (req, res) => {
+  res.render("skills.ejs");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+app.get("/Aboutme", (req, res) => {
+  res.render("Aboutme.ejs");
+});
+
+
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
+
+
+ 
