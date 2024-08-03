847692a44223ba4a8de4f21b278aa73712af90ba
Gyaneswar-1 <gyaneswarrout12345@gmail.com>
2024-08-03 18:18:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+
 app.get("/index", (req, res) => {
   res.render("index.ejs");
 });
